4159cf0556cba1116c62bbef362405d391375f7d
Alessandro Ristori <81309500+RistoAle97@users.noreply.github.com>
2022-10-30 18:03:30 +0100

    Implemented damage calculator

---
+++ battle_utilities.py
@@ -0,0 +1,114 @@
+from poke_env.environment import Pokemon, Move, Weather, Field, Status
+from poke_env.environment.move_category import MoveCategory
+
+
+def compute_damage(move: Move,
+                   attacker: Pokemon,
+                   defender: Pokemon,
+                   weather_list: list,
+                   field_list: list,
+                   is_bot_turn: bool) -> float:
+    if move.category is MoveCategory.STATUS:
+        return 0
+
+    move_type = move.type
+
+    # Compute the effect of the attacker level
+    level_multiplier = 2 * attacker.level / 5 + 2
+
+    # Compute the ratio between the attacker atk/spa stat and the defender def/spd stat
+    if move.category is MoveCategory.PHYSICAL:
+        if is_bot_turn:
+            attack_stat = attacker.stats["atk"]
+            defense_stat = defender.base_stats["def"]
+        else:
+            attack_stat = attacker.base_stats["atk"]
+            defense_stat = defender.stats["def"]
+    else:
+        if is_bot_turn:
+            attack_stat = attacker.stats["spa"]
+            defense_stat = defender.base_stats["spd"]
+        else:
+            attack_stat = attacker.base_stats["spa"]
+            defense_stat = defender.stats["spd"]
+
+    if is_bot_turn:
+        defense_stat = 2 * defense_stat + 16  # assume average IVs
+        defense_stat = defense_stat + 21  # assume that EVs are spread equally between all the stats
+        defense_stat = defense_stat * defender.level / 100 + 5  # assume that nature is neutral
+        ratio_attack_defense = attack_stat / int(defense_stat)
+    else:
+        attack_stat = 2 * attack_stat + 16  # assume average IVs
+        attack_stat = attack_stat + 21  # assume that EVs are spread equally between all the stats
+        attack_stat = attack_stat * defender.level / 100 + 5  # assume that nature is neutral
+        ratio_attack_defense = int(attack_stat) / defense_stat
+
+    print("Pokemon: {0}, {1} Attack: {2}".format(attacker.species, move.category.name, attack_stat))
+    print("Pokemon: {0}, Appr. {1} Defense: {2}\n".format(defender.species, move.category.name, defense_stat))
+
+    # Compute the base damage before taking into account any modifier
+    damage = level_multiplier * move.base_power * ratio_attack_defense / 50 + 2
+
+    # Compute the effect of the weather
+    weather_multiplier = 1
+    if len(weather_list) > 0:
+        weather = weather_list[0]
+        if weather in [Weather.SUNNYDAY, Weather.DESOLATELAND]:
+            if move_type == "Fire":
+                weather_multiplier = 1.5
+            elif move_type == "Water":
+                weather_multiplier = 0.5
+                if weather is Weather.DESOLATELAND:
+                    return 0
+        elif weather in [Weather.RAINDANCE, Weather.PRIMORDIALSEA]:
+            if move_type == "Water":
+                weather_multiplier = 1.5
+            elif move_type == "Fire":
+                weather_multiplier = 0.5
+                if weather is Weather.PRIMORDIALSEA:
+                    return 0
+
+    damage = damage * weather_multiplier
+
+    # Compute the effect of the field
+    field_multiplier = 1
+    if len(field_list) > 0:
+        field = field_list[0]
+        if field is Field.ELECTRIC_TERRAIN:
+            if move_type == "Electric":
+                field_multiplier = 1.3
+        elif field is Field.GRASSY_TERRAIN:
+            if move_type == "Grass":
+                field_multiplier = 1.3
+            elif move.id in ["earthquake", "magnitude", "bulldoze"]:
+                field_multiplier = 0.5
+        elif field is Field.MISTY_TERRAIN:
+            if move_type == "Dragon":
+                field_multiplier = 0.5
+        elif field is Field.PSYCHIC_TERRAIN:
+            if move_type == "Psychic":
+                field_multiplier = 1.3
+            elif move.priority > 0:
+                return 0
+
+    damage = damage * field_multiplier
+
+    # Compute effect of the STAB
+    if move_type in attacker.types:
+        if attacker.ability == "Adaptability":
+            stab_multiplier = 2
+        else:
+            stab_multiplier = 1.5
+    else:
+        stab_multiplier = 1
+
+    damage = damage * stab_multiplier
+
+    # Compute the effect of the burn
+    if attacker.status is Status.BRN and move.category is MoveCategory.PHYSICAL and attacker.ability != "Guts":
+        damage = damage * 0.5
+
+    # Compute the effect of the defender's types
+    type_multiplier = defender.damage_multiplier(move)
+    damage = damage * type_multiplier
+    return damage
