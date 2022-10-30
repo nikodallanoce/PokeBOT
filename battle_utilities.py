f6cfa68bf4c22893757f127d7dbbb86d4617895a
Alessandro Ristori <81309500+RistoAle97@users.noreply.github.com>
2022-10-30 22:30:23 +0100

    Updated damage calculator

--- battle_utilities.py
+++ battle_utilities.py
@@ -2,12 +2,21 @@ from poke_env.environment import Pokemon, Move, Weather, Field, Status
 from poke_env.environment.move_category import MoveCategory
 
 
+def _compute_boost_to_stat(pokemon: Pokemon, stat: str) -> float:
+    if pokemon.boosts[stat] > 0:
+        stat_boost = (2 + pokemon.boosts[stat]) / 2
+    else:
+        stat_boost = 2 / (2 - pokemon.boosts[stat])
+
+    return stat_boost
+
+
 def compute_damage(move: Move,
                    attacker: Pokemon,
                    defender: Pokemon,
                    weather_list: list,
                    field_list: list,
-                   is_bot_turn: bool) -> float:
+                   is_bot: bool) -> int:
     if move.category is MoveCategory.STATUS:
         return 0
 
@@ -18,33 +27,37 @@ def compute_damage(move: Move,
 
     # Compute the ratio between the attacker atk/spa stat and the defender def/spd stat
     if move.category is MoveCategory.PHYSICAL:
-        if is_bot_turn:
+        boosts_to_consider = ["atk", "def"]
+        if is_bot:
             attack_stat = attacker.stats["atk"]
             defense_stat = defender.base_stats["def"]
         else:
             attack_stat = attacker.base_stats["atk"]
             defense_stat = defender.stats["def"]
     else:
-        if is_bot_turn:
+        boosts_to_consider = ["spa", "spd"]
+        if is_bot:
             attack_stat = attacker.stats["spa"]
             defense_stat = defender.base_stats["spd"]
         else:
             attack_stat = attacker.base_stats["spa"]
             defense_stat = defender.stats["spd"]
 
-    if is_bot_turn:
-        defense_stat = 2 * defense_stat + 16  # assume average IVs
+    if is_bot:
+        defense_stat = 2 * defense_stat + 31  # assume max IVs
         defense_stat = defense_stat + 21  # assume that EVs are spread equally between all the stats
-        defense_stat = defense_stat * defender.level / 100 + 5  # assume that nature is neutral
-        ratio_attack_defense = attack_stat / int(defense_stat)
+        defense_stat = defense_stat * defender.level / 100 + 5  # assume that the nature is neutral
     else:
-        attack_stat = 2 * attack_stat + 16  # assume average IVs
+        attack_stat = 2 * attack_stat + 31  # assume max IVs
         attack_stat = attack_stat + 21  # assume that EVs are spread equally between all the stats
-        attack_stat = attack_stat * defender.level / 100 + 5  # assume that nature is neutral
-        ratio_attack_defense = int(attack_stat) / defense_stat
+        attack_stat = attack_stat * attacker.level / 100 + 5  # assume that nature is neutral
 
-    print("Pokemon: {0}, {1} Attack: {2}".format(attacker.species, move.category.name, attack_stat))
-    print("Pokemon: {0}, Appr. {1} Defense: {2}\n".format(defender.species, move.category.name, defense_stat))
+    attack_stat = attack_stat * _compute_boost_to_stat(attacker, boosts_to_consider[0])
+    defense_stat = defense_stat * _compute_boost_to_stat(defender, boosts_to_consider[1])
+    ratio_attack_defense = attack_stat / defense_stat
+    print("Move under evaluation: {0}".format(move.id))
+    print("Pokemon: {0}, {1} Attack: {2}".format(attacker.species, move.category.name, int(attack_stat)))
+    print("Pokemon: {0}, Appr. {1} Defense: {2}".format(defender.species, move.category.name, int(defense_stat)))
 
     # Compute the base damage before taking into account any modifier
     damage = level_multiplier * move.base_power * ratio_attack_defense / 50 + 2
@@ -110,5 +123,6 @@ def compute_damage(move: Move,
 
     # Compute the effect of the defender's types
     type_multiplier = defender.damage_multiplier(move)
-    damage = damage * type_multiplier
+    damage = int(damage * type_multiplier)
+    print("Damage: {0}\n".format(damage))
     return damage
