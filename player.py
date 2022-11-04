141271cb047f25a88e0bcaa34f4116ba6d0801d9
Niko Dalla Noce <dllnik97@gmail.com>
2022-11-04 12:34:27 +0100

    matchup rewritten using move types

--- player.py
+++ player.py
@@ -1,4 +1,4 @@
-from poke_env.environment import Move, Pokemon, Battle, Weather, Field
+from poke_env.environment import Move, Pokemon, Battle, Weather, Field, MoveCategory
 from poke_env.player import Player
 from battle_utilities import compute_damage, outspeed_prob
 
@@ -16,7 +16,7 @@ class BestDamagePlayer(Player):
     def choose_move(self, battle):
         bot_pokemon: Pokemon = battle.active_pokemon
         opponent_pokemon: Pokemon = battle.opponent_active_pokemon
-        self.matchup(battle)
+        self.matchup_on_types(battle)
         if battle.available_moves:
             if self.verbose:
                 print("Turn {0}".format(battle.turn))
@@ -65,29 +65,37 @@ class BestDamagePlayer(Player):
 
             return self.choose_random_move(battle)
 
-    def matchup(self, battle):
-        outspeed_p = outspeed_prob(battle.active_pokemon, battle.opponent_active_pokemon)
-        type_adv = self.type_advantage(battle.active_pokemon, battle.opponent_active_pokemon)
+    def matchup_on_types(self, battle):
+        # outspeed_p = outspeed_prob(battle.active_pokemon, battle.opponent_active_pokemon)
+        bot_type_adv = self.type_advantage(battle.active_pokemon, battle.opponent_active_pokemon)
+        opponent_type_adv = self.type_advantage(battle.opponent_active_pokemon, battle.active_pokemon)
+        poke_adv = bot_type_adv - opponent_type_adv
+        move_adv = self.move_type_advantage(battle.active_pokemon, battle.opponent_active_pokemon, opponent_type_adv)
+        return poke_adv + move_adv
 
-        weather = None if len(battle.weather.keys()) == 0 else next(iter(battle.weather.keys()))
-        terrain = None if len(battle.fields.keys()) == 0 else next(iter(battle.fields.keys()))
+    def move_type_advantage(self, bot_pokemon: Pokemon, opponent_pokemon: Pokemon, opponent_type_adv: float) -> float:
 
-        move_damage_att = self.move_with_max_damage(battle.active_pokemon, battle.opponent_active_pokemon, weather, terrain,
-                                                True)
-        move_damage_def = self.move_with_max_damage(battle.opponent_active_pokemon, battle.active_pokemon, weather, terrain,
-                                                False)
-
-        print()
-
-    def type_advantage(self, bot_pokemon: Pokemon, opponent_pokemon: Pokemon) -> float:
         # Consider the bot type match-up
-        bot_type_gain = max([opponent_pokemon.damage_multiplier(bot_type)
-                             for bot_type in bot_pokemon.types if bot_type is not None])
+        bot_type_gain = max([opponent_pokemon.damage_multiplier(move_bot)
+                             for move_bot in bot_pokemon.moves.values() if
+                             move_bot.category is not MoveCategory.STATUS])
 
-        # Consider the opponent type match-up
-        opponent_type_gain = max([bot_pokemon.damage_multiplier(opponent_type)
-                                  for opponent_type in opponent_pokemon.types if opponent_type is not None])
-        type_gain = bot_type_gain - opponent_type_gain
+        opponent_type_gain = 0
+        if len(opponent_pokemon.moves) > 0:
+            for move_opp in opponent_pokemon.moves.values():
+                if move_opp.category is not MoveCategory.STATUS:
+                    opponent_type_gain_iter = bot_pokemon.damage_multiplier(move_opp)
+                    if opponent_type_gain_iter > opponent_type_gain:
+                        opponent_type_gain = opponent_type_gain_iter
+
+        if opponent_type_gain < opponent_type_adv and len(opponent_pokemon.moves) < 4:
+            return bot_type_gain - opponent_type_adv
+        return bot_type_gain - opponent_type_gain
+
+    def type_advantage(self, attacker: Pokemon, defender: Pokemon) -> float:
+        # Consider the bot type match-up
+        type_gain = max([defender.damage_multiplier(attacker_type)
+                         for attacker_type in attacker.types if attacker_type is not None])
         return type_gain
 
     def move_with_max_damage(self, attacker: Pokemon, defender: Pokemon, weather: Weather, terrain: Field,
