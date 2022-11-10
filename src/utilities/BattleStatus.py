7b61cde8b23dbdf877ceb55e053a41eec84d64d4
Niko Dalla Noce <dllnik97@gmail.com>
2022-11-10 04:09:57 +0100

    minimax implemented with casual errors

---
+++ src/utilities/BattleStatus.py
@@ -0,0 +1,61 @@
+from poke_env.player import Player, BattleOrder
+from poke_env.environment import Pokemon, Battle, Move, MoveCategory, Weather, Field, Status, SideCondition, \
+    AbstractBattle
+
+from src.utilities.NodePokemon import NodePokemon
+from src.utilities.battle_utilities import *
+from src.utilities.stats_utilities import *
+
+
+class BattleStatus:
+
+    def __init__(self, act_poke: NodePokemon, opp_poke: NodePokemon,
+                 avail_moves: list[Move],
+                 avail_switches: list[Pokemon],
+                 weather: Weather, terrain: Field, ancestor, move: Move):
+        self.act_poke = act_poke
+        self.opp_poke = opp_poke
+        self.avail_moves = avail_moves
+        self.avail_switches = avail_switches
+        self.weather = weather
+        self.terrain = terrain
+        self.ancestor = ancestor
+        self.score = 0
+        self.move = move
+
+    def act_poke_avail_actions(self):
+        return self.avail_moves
+
+    def opp_poke_avail_actions(self):
+        return self.opp_poke.moves
+
+    def compute_score(self):
+        act_poke_hp = self.act_poke.current_hp
+        opp_poke_hp = self.opp_poke.current_hp
+        if act_poke_hp < 0:
+            act_poke_hp = 0
+        if opp_poke_hp < 0:
+            opp_poke_hp = 0
+        opp_poke_max_hp = estimate_stat(self.opp_poke.pokemon, "hp")
+        score = (act_poke_hp / self.act_poke.pokemon.max_hp) - (opp_poke_hp / opp_poke_max_hp)
+        return score
+
+    def simulate_turn(self, move: Move, is_my_turn: bool):
+        if is_my_turn:
+            damage = compute_damage(move, self.act_poke.pokemon, self.opp_poke.pokemon, self.weather, self.terrain,
+                                    is_my_turn)
+            updated_hp = self.opp_poke.current_hp - damage
+            opp_poke = self.opp_poke.clone(current_hp=updated_hp)
+
+            child = BattleStatus(self.act_poke, opp_poke,
+                                 self.avail_moves.copy(), self.avail_switches, self.weather, self.terrain, self, move)
+            return child
+
+        else:
+            damage = compute_damage(move, self.opp_poke.pokemon, self.act_poke.pokemon, self.weather, self.terrain,
+                                    is_my_turn)
+            updated_hp = self.opp_poke.current_hp - damage
+            act_poke = self.act_poke.clone(current_hp=updated_hp)
+            # self.score = self.compute_score(updated_hp, self.opp_poke_hp)
+            return BattleStatus(act_poke, self.opp_poke, self.avail_moves.copy(), self.avail_switches,
+                                self.weather, self.terrain, self, move)
---
+++ src/utilities/NodePokemon.py
@@ -0,0 +1,79 @@
+from poke_env.environment import Pokemon, Battle, Move, MoveCategory, Weather, Field, Status, SideCondition, \
+    AbstractBattle
+from poke_env.environment.pokemon_type import PokemonType as pt
+from poke_env.environment.move import Gen8Move
+
+
+class NodePokemon:
+    default_moves = {pt.ROCK: Gen8Move('powergem'),
+                     pt.BUG: Gen8Move('xscissor'),
+                     pt.DARK: Gen8Move('nightslash'),
+                     pt.DRAGON: Gen8Move('dragonpulse'),
+                     pt.ELECTRIC: Gen8Move('thunderbolt'),
+                     pt.FAIRY: Gen8Move('moonblast'),
+                     pt.FIGHTING: Gen8Move('bodypress'),
+                     pt.FIRE: Gen8Move('flamethrower'),
+                     pt.FLYING: Gen8Move('drillpeck'),
+                     pt.GHOST: Gen8Move('moongeistbeam'),
+                     pt.GRASS: Gen8Move('energyball'),
+                     pt.GROUND: Gen8Move('earthquake'),
+                     pt.ICE: Gen8Move('icebeam'),
+                     pt.NORMAL: Gen8Move('bodyslam'),
+                     pt.POISON: Gen8Move('shellsidearm'),
+                     pt.PSYCHIC: Gen8Move('psychic'),
+                     pt.STEEL: Gen8Move('flashcannon'),
+                     pt.WATER: Gen8Move('surf')
+                     }
+
+    def __init__(self, pokemon: Pokemon, current_hp: int = None, current_stats: dict[str, int] = None,
+                 status: Status = None, moves: list[Move] = None):
+        self.pokemon = pokemon
+
+        if current_hp is None:
+            current_hp = pokemon.current_hp
+        self.current_hp = current_hp
+
+        if current_stats is None:
+            current_stats = pokemon.stats
+        self.current_stats = current_stats
+
+        if status is None:
+            status = pokemon.status
+        self.status = status
+
+        if moves is None:
+            if len(pokemon.moves) == 4:
+                moves = pokemon.moves
+            else:
+                moves = self.enrich_moves(list(pokemon.moves.values()))
+
+        self.moves = moves
+
+    def clone_all(self):
+        return NodePokemon(self.pokemon, self.current_hp, self.current_stats, self.status)
+
+    def clone(self, current_hp: int = None, current_stats: dict[str, int] = None, status: Status = None,
+              moves: list[Move] = None):
+        if current_hp is None:
+            current_hp = self.current_hp
+        if current_stats is None:
+            current_stats = self.current_stats
+        if status is None:
+            status = self.status
+        if moves is None:
+            moves = self.moves
+        return NodePokemon(self.pokemon, current_hp, current_stats, status)
+
+    def enrich_moves(self, known_moves: list[Move]) -> list[Move]:
+        moves_added: list[Move] = []
+        for poke_type in iter(self.pokemon.types):
+            if poke_type is not None:
+                move_same_poke_type = False
+                for move in known_moves:
+                    if move.type == poke_type:
+                        move_same_poke_type = True
+                        break
+                if not move_same_poke_type:
+                    def_move: Gen8Move = self.default_moves[poke_type]
+                    moves_added.append(def_move)
+        return moves_added + known_moves
