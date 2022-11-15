e21a8354ee99827552777ab929bdec119c2f7f16
Niko Dalla Noce <dllnik97@gmail.com>
2022-11-15 18:08:25 +0100

    Update Heuristic.py
    Co-Authored-By: Giuseppe Lombardi <74201383+icezimmer@users.noreply.github.com>

--- src/utilities/Heuristic.py
+++ src/utilities/Heuristic.py
@@ -1,13 +1,10 @@
 from abc import ABC, abstractmethod
 
-from src.utilities.BattleStatus import BattleStatus
-
-
 class Heuristic(ABC):
 
     def __init__(self):
         super(Heuristic, self).__init__()
 
     @abstractmethod
-    def compute(self, battle_node: BattleStatus) -> float:
+    def compute(self, battle_node) -> float:
         pass
